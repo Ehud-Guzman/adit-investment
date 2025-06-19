9aecb9c3cc27c24075498e2734c79d27f77fd23f
Ehud Guzman <nyamuehud@gmail.com>
2025-06-19 14:16:00 +0300

    Folder change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // this is what Render will deploy
+  },
   server: {
     port: 5173,
-    open: true,
-  },
-  build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 1500,
-  },
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
   },
 });
