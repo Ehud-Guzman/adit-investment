70c385c375e6a42a89aa44b2edb23c9ef7409ab6
Ehud Guzman <nyamuehud@gmail.com>
2025-06-23 12:37:42 +0300

    Final touch

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   plugins: [
     react(),
-    
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.', // copy to root of dist
+        },
+      ],
+    }),
   ],
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Render deploys this folder
   },
   server: {
     port: 5173,
